40e9442200916103c77a1f53f2d7fe4dbafb77eb
illyanyc <illya.n@me.com>
2021-07-12 15:02:29 -0400

    LSTM model complete, updated technical indicators

--- code/archive/backtests/backtest.py
+++ code/archive/backtests/backtest.py
@@ -12,7 +12,7 @@ dateformat = "%Y-%m-%d"
 from pos_sizers import LongOnly
 
 def optimization(strat,
-                 ticker : str,
+                 ohlcv : pd.DataFrame,
                  start_cash : float,
                  broker_comm : float,
                  risk : float,
@@ -23,8 +23,6 @@ def optimization(strat,
                  end_date):
     
     start_time = datetime.now(IST)
-    
-    ohlcv = yh.ohlcv(ticker, start_date, end_date)
 
     # parse ohlcv data into PandasData 
     data = bt.feeds.PandasData(dataname=ohlcv)
@@ -52,10 +50,10 @@ def optimization(strat,
     opt_run = cerebro.run()
 
     # calculate hold open
-    hold_size = int((start_cash * risk) / ohlcv.iloc[0]['Open'])
-    hold_open = (hold_size * ohlcv.iloc[0]['Open']) - (hold_size * broker_comm)
+    hold_size = int((start_cash * risk) / ohlcv.iloc[0]['open'])
+    hold_open = (hold_size * ohlcv.iloc[0]['open']) - (hold_size * broker_comm)
     # calculate hold close
-    hold_close = (hold_size * ohlcv.iloc[-1]['Close']) - (hold_size * broker_comm)
+    hold_close = (hold_size * ohlcv.iloc[-1]['close']) - (hold_size * broker_comm)
     # calculate hold pnl
     hold_pnl = round((hold_close - hold_open),2)
 
@@ -71,9 +69,13 @@ def optimization(strat,
             period_instance = strategy.params.period
             upperband_instance = strategy.params.upperband
             lowerband_instance = strategy.params.lowerband
+            
+            alpha_bin = np.where(result_df['alpha'] > 0, 1, 0)
 
-            results = [period_instance, upperband_instance, lowerband_instance,trade_pnl,hold_pnl,alpha,pct_alpha]
+            results = [period_instance, upperband_instance, lowerband_instance,trade_pnl,hold_pnl,alpha,pct_alpha,alpha_bin]
             final_results_list.append(results)
+            
+            
 
     result_df = pd.DataFrame(final_results_list, 
               columns=['period',
@@ -82,7 +84,8 @@ def optimization(strat,
                        'trade_pnl',
                        'hold_pnl',
                        'alpha',
-                       'pct_alpha'])
+                       'pct_alpha',
+                       'alpha_bin'])
     
     end_time = datetime.now(IST)
     delta = end_time - start_time
